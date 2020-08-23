602c5a9a6ed1f60f39a0f47892d83881625f5638
oanatand <oana.tand@gmail.com>
2020-08-23 22:01:20 +0300

    small changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,27 +19,29 @@ class Projects extends Component {
 
     renderProjects() {
         let projectsDomData = [];
+        let j=0;
         for (let i = 0; i < ProjectsData.length / 2; i++) {
             projectsDomData.push(
                 <Row key={i}>
                     <Col className="col-4">
-                        <ProjectContainer projectTitle={ProjectsData[i].projectTitle}
-                            projectDescription={ProjectsData[i].projectDescription}
-                            projectGitHubUrl={ProjectsData[i].projectGitHubUrl}
-                            projectGitHubPageUrl={ProjectsData[i].projectGitHubPageUrl}
+                        <ProjectContainer projectTitle={ProjectsData[j].projectTitle}
+                            projectDescription={ProjectsData[j].projectDescription}
+                            projectGitHubUrl={ProjectsData[j].projectGitHubUrl}
+                            projectGitHubPageUrl={ProjectsData[j].projectGitHubPageUrl}
                             classIndex={i + 1}>
                         </ProjectContainer>
                     </Col>
                     <Col className="col-4">
-                        <ProjectContainer projectTitle={ProjectsData[i + 1].projectTitle}
-                            projectDescription={ProjectsData[i + 1].projectDescription}
-                            projectGitHubUrl={ProjectsData[i + 1].projectGitHubUrl}
-                            projectGitHubPageUrl={ProjectsData[i + 1].projectGitHubPageUrl}
+                        <ProjectContainer projectTitle={ProjectsData[j + 1].projectTitle}
+                            projectDescription={ProjectsData[j + 1].projectDescription}
+                            projectGitHubUrl={ProjectsData[j + 1].projectGitHubUrl}
+                            projectGitHubPageUrl={ProjectsData[j + 1].projectGitHubPageUrl}
                             classIndex={i + 1}>
                         </ProjectContainer>
                     </Col>
                 </Row>
             )
+            j=j+2;
         }
         return projectsDomData;
     }
